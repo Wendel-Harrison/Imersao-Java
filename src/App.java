2fed9bf835616ce3f788a0e84a8cbcdeacfa90c4
wendel <wh68639@gmail.com>
2023-03-31 19:22:18 -0300

    finalização da fabrica de stickers, aula 03

--- src/App.java
+++ src/App.java
@@ -1,10 +1,5 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
@@ -12,33 +7,32 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         // fazer uma conexão HTTP e buscar o top250 filmes
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json"; //url da API
-        URI endereco = URI.create(url); // criamos a URI que dentro recebe a url
-        var client = HttpClient.newHttpClient(); // criamos o cliente que recebe um novo cliente com o protocolo HTTP
-        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build(); //fizemos a requisição com o newBuider recebendo a url com o método GET
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString()); //a resposta veio através fo body em formato de string
-        String body = response.body(); // pegamos o body e colocamos dentro da variavel body
-         //chamamos a body
+            // String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json"; //url da API
+        // ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMdb();
+       
+        String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2023-01-10&end_date=2023-01-17";
+        ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
+
+
+       var http = new ClienteHttp();
+       String json = http.buscaDados(url);
         
-        // extrair só os dados que interessa para nós (titulo, poster, rating)
-        var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
-        
+    
         // exibir e manipular os dados 
         
-        
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
         var fabrica = new fabricaDeFigurinhas();
-        for (Map<String,String> filme : listaDeFilmes) {
-            String urlImagem = filme.get("image");
-            String titulo = filme.get("title");
+        for (int i = 0; i < 7; i++) {
 
-            InputStream inputStream = new URL(urlImagem).openStream();
+           Conteudo conteudo = conteudos.get(i);
 
-            String nomeArquivo = "./saida/" +titulo + ".png";
+            InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+
+            String nomeArquivo = "./saida/" +conteudo.getTitulo() + ".png";
             fabrica.cria(inputStream, nomeArquivo);
 
 
-            System.out.println(titulo);
+            System.out.println(conteudo.getTitulo());
             System.out.println();
             
         }
